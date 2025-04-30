b321764786dc2096baff5477e60a807f25c966fd
Lilia Lin <lilia.lin@example.com>
2025-04-30 22:03:20 +0100

    move object with speed 0.5f

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Vector3 goal = new Vector3(0, 0, 0);
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
+        // disminuir el paso de movimiento
+        goal = goal * 0.5f;
+    }
+    
+    void Update()
+    {
+        // disminuir el paso de movimiento
+        goal = goal * 0.5f;
+        
         // move goal
         this.transform.Translate(goal);
         Debug.Log("Goal has moved, check new position");

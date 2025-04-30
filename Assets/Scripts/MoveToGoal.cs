768ea55a7aa6d4c6112921ec6abe789899042b7f
Lilia Lin <lilia.lin@example.com>
2025-05-01 00:36:58 +0100

    finished exercise 1

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlaneMovementWithGoal : MonoBehaviour
 {
-    public Vector3 goal = new Vector3(0, 0, 0);
+    public Vector3 goal = new Vector3(3,0,0);
+    public Vector3 flygoal = new Vector3(5,3,0);
+    public Vector3 flyInSkygoal = new Vector3(1, 0, 0);
+    
+    public float speed = 0.5f;
+    public float flySpeed = 0.3f;
 
+    private float timer = 0f;
 
     void Update()
     {
-        // move goal
-        this.transform.Translate(goal);
+        // to count timer
+        timer += Time.deltaTime;
 
-        // disminuir el paso de movimiento
-        goal = goal * 0.5f;
-
-        Debug.Log("Goal has moved, check new position");
+        if(timer < 2f)
+        {
+            // * Time.deltaTime is for the speed
+            this.transform.Translate(goal * speed * Time.deltaTime);
+        } else if(timer > 2f && timer < 4f) {
+            this.transform.Translate(flygoal * flySpeed * Time.deltaTime);
+        } else {
+            this.transform.Translate(flyInSkygoal * flySpeed * Time.deltaTime);
+        }
     }
 }

4f82a63383ddebed2ab4b43d014e64b36eab407a
Lilia Lin <lilia.lin@example.com>
2025-04-30 22:25:00 +0100

    change Y to 0

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -4,19 +4,15 @@ public class PlayerController : MonoBehaviour
 {
     public Vector3 goal = new Vector3(0, 0, 0);
 
-    void Start()
-    {
-        // disminuir el paso de movimiento
-        goal = goal * 0.5f;
-    }
-    
+
     void Update()
     {
-        // disminuir el paso de movimiento
-        goal = goal * 0.5f;
-        
         // move goal
         this.transform.Translate(goal);
+
+        // disminuir el paso de movimiento
+        goal = goal * 0.5f;
+
         Debug.Log("Goal has moved, check new position");
     }
 }

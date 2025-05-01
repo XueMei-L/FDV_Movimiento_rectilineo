839496ab47789b18297770bb1162bcb55940e52e
Lilia Lin <lilia.lin@example.com>
2025-05-01 01:03:46 +0100

    use the normalized direction vector

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -2,28 +2,14 @@ using UnityEngine;
 
 public class PlaneMovementWithGoal : MonoBehaviour
 {
-    public Vector3 goal = new Vector3(3,0,0);
-    public Vector3 flygoal = new Vector3(5,3,0);
-    public Vector3 flyInSkygoal = new Vector3(1, 0, 0);
-    
-    public float speed = 0.5f;
-    public float flySpeed = 0.3f;
-
-    private float timer = 0f;
+    public Vector3 goal = new Vector3(0,0,0);
 
     void Update()
     {
-        // to count timer
-        timer += Time.deltaTime;
 
-        if(timer < 2f)
-        {
-            // * Time.deltaTime is for the speed
-            this.transform.Translate(goal * speed * Time.deltaTime);
-        } else if(timer > 2f && timer < 4f) {
-            this.transform.Translate(flygoal * flySpeed * Time.deltaTime);
-        } else {
-            this.transform.Translate(flyInSkygoal * flySpeed * Time.deltaTime);
-        }
+        // Use the normalized direction vector
+        goal *= .5f;
+    
+        this.transform.Translate(goal.normalized);
     }
 }

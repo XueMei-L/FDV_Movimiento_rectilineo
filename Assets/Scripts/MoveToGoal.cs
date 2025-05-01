3e516a1c7b198426342638e635f28954d84149ed
Lilia Lin <lilia.lin@example.com>
2025-05-01 22:02:32 +0100

    execise 3

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
     public class PlaneMovementWithGoal : MonoBehaviour
     {
-        public Vector3 goal = new Vector3(0,0,0);
-        public float speed = 0.1f;
+        public Transform goal;
+        public float speed = 1.0f;
 
         void Update()
         {

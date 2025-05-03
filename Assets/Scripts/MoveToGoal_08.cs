3230155e49c23039b7873b1ce23c831fb9b757e7
Lilia Lin <lilia.lin@example.com>
2025-05-03 01:47:27 +0100

    execise 8

--- Assets/Scripts/MoveToGoal_08.cs
+++ Assets/Scripts/MoveToGoal_08.cs
@@ -1,32 +1,37 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class PlaneMovementWithGoal : MonoBehaviour
-// {
-//     public Transform goal;
-//     public float baseSpeed = 1.0f;
-//     public float accuracy = 0.01f;
+public class PlaneMovementWithGoal : MonoBehaviour
+{
+    public Transform goal;
+    public float baseSpeed = 1.0f;
+    public float accuracy = 0.01f;
 
-//     void Start()
-//     {
-//         this.transform.LookAt(goal.position);
-//     }
+    void Start()
+    {
+        this.transform.LookAt(goal.position);
+    }
 
-//     void Update()
-//     {
-//         // 1. Calcular dirección normalizada en espacio mundial
-//         Vector3 direction = goal.position - transform.position;
+    void Update()
+    {
+        // 1. Calcular dirección normalizada en espacio mundial
+        Vector3 direction = goal.position - transform.position;
         
-//         // 2. Si está demasiado cerca, no mover
-//         if (Vector3.Distance(transform.position, goal.position) <= accuracy) return;
+        // 2. Si está demasiado cerca, no mover
+        if (Vector3.Distance(transform.position, goal.position) <= accuracy) return;
 
-//         // Move the object forward along its z axis 1 unit/second.
-//         // 3. Rotar y mover
-//         this.transform.LookAt(goal.position);
-//         this.transform.Translate(
-//             direction.normalized * baseSpeed * Time.deltaTime,
-//             Space.World);
+        // 3. Rotar
+        transform.rotation = Quaternion.Slerp(
+            Quaternion.LookRotation(transform.forward),  // Current forward
+            Quaternion.LookRotation(direction),         // Target direction
+            baseSpeed * Time.deltaTime
+        );
+
+        // 4. mover
+        this.transform.Translate(
+            direction.normalized * baseSpeed * Time.deltaTime,
+            Space.World);
         
-//         // Debug: línea roja muestra dirección mundial
-//         Debug.DrawRay(this.transform.position, direction, Color.red);
-//     }
-// }
+        // 5. Debug: línea roja muestra dirección mundial
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+    }
+}
